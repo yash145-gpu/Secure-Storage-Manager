e9624ab1df0be7dbbf622f4d9975d8b24ab60f63
yash145-gpu <yashps13101@gmail.com>
2025-01-30 22:37:33 +0530

    Updated File operations , panel

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class FileManager {
     protected static File currentDirectory;
 
-    static void listFiles(File directory, JPanel filePanel, JButton backButton, JTextArea feedbackArea, GUI gui) {
+    static void listFiles(File directory, JPanel filePanel, JButton backButton) {
         filePanel.removeAll();
         currentDirectory = directory;
         backButton.setEnabled(directory.getParentFile() != null);
@@ -18,10 +18,10 @@ public class FileManager {
                 fileButton.setPreferredSize(new Dimension(30, 100));
                 fileButton.addActionListener(e -> {
                     if (file.isDirectory()) {
-                        listFiles(file, filePanel, backButton, feedbackArea, gui);
+                        listFiles(file, filePanel, backButton);
                     } else {
                         String[] options = { "Save to DB", "Encrypt and Save", "SHA File Checksum", "Cancel" };
-                        int choice = JOptionPane.showOptionDialog(gui,
+                        int choice = JOptionPane.showOptionDialog(null,
                                 "What would you like to do with the file?",
                                 "File Action",
                                 JOptionPane.DEFAULT_OPTION,
@@ -31,19 +31,19 @@ public class FileManager {
                                 options[0]);
                         switch (choice) {
                             case 0:
-                                BackupManager.saveFileToDatabase(feedbackArea, file);
+                                BackupManager.saveFileToDatabase(file);
                                 break;
                             case 1:
-                                SecurityTools.encryptFileToDatabase(feedbackArea, file);
+                                SecurityTools.encryptFileToDatabase(file);
                                 break;
                             case 2:
                                 String[] shs = { "SHA256", "SHA512" };
-                                int ch = JOptionPane.showOptionDialog(gui,"Please Select Secure Hashing Algorithm ",  "Select SHA Algorithm",
+                                int ch = JOptionPane.showOptionDialog(null,"Please Select Secure Hashing Algorithm ",  "Select SHA Algorithm",
                                         JOptionPane.DEFAULT_OPTION,
                                         JOptionPane.QUESTION_MESSAGE, null, shs,
                                         shs[0]);
                                         if(ch==JOptionPane.CLOSED_OPTION || ch == JOptionPane.CANCEL_OPTION){return;}
-                                        SecurityTools.hashfile(file, feedbackArea, ch);
+                                        SecurityTools.hashfile(file, ch);
                                     break;    
 
                                     }
@@ -56,9 +56,9 @@ public class FileManager {
         filePanel.repaint();
     }
 
-    protected static void navigateBack(JPanel filePanel, JButton backButton, JTextArea feedbackArea, GUI gui) {
+    protected static void navigateBack(JPanel filePanel, JButton backButton) {
         if (currentDirectory != null && currentDirectory.getParentFile() != null) {
-            listFiles(currentDirectory.getParentFile(), filePanel, backButton, feedbackArea, gui);
+            listFiles(currentDirectory.getParentFile(), filePanel, backButton);
         }
     }
 }
